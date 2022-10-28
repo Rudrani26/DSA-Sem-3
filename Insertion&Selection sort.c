7e7fb32ddda55ac26fdfe3dc50377182318e0ac9
Rudrani26 <66739928+Rudrani26@users.noreply.github.com>
2022-10-29 01:09:07 +0530

    Insertion and Selection Sort

---
+++ Insertion&Selection sort.c
@@ -0,0 +1,89 @@
+#include<stdio.h>
+
+void insertion()
+{
+    int i,j,low,n;
+    printf("\nEnter size of array: ");
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+   {
+        printf("Enter Element : ",i+1);
+        scanf("%d",&a[i]);
+   }
+    for(i=1;i<n;i++)
+    {
+        low=a[i];
+        j=i-1;
+
+        display(a,n);
+
+        while(j>=0 && low<a[j] )
+        {
+            a[j+1]=a[j];
+            j=j-1;
+        }
+        a[j+1]=low;
+    }
+
+}
+void selection()
+{
+     int i,j,temp,n;
+    printf("\nEnter size of array: ");
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+   {
+        printf("Enter Element : ",i+1);
+        scanf("%d",&a[i]);
+   }
+    for(i=0;i<n-1;i++)
+    {
+       display(a,n);
+
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+             temp=a[i];
+             a[i]=a[j];
+             a[j]=temp;
+            }
+        }
+    }
+}
+void display(int a[],int n)
+{
+    printf("\n");
+    for(int i=0;i<n;i++)
+   {
+        printf(" %d ",a[i]);
+   }
+    printf("\n");
+}
+int main()
+{
+    int ch;
+   do{
+   printf("\n1.Insertion sort\n2.Selection Sort\n3.Exit\n");
+   printf("Enter your choice: ");
+   scanf("%d",&ch);
+
+   switch(ch)
+   {
+       case 1:
+           //printf("Insertion sort");
+           insertion();
+        break;
+       case 2:
+           //printf("Selection sort");
+           selection();
+        break;
+       case 3:
+        exit(0);
+   }
+   }while(ch!=3);
+
+   return 0;
+}
